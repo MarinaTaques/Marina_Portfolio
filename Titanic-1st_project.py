e433a3f3a9d1f4bde0fff5a328f602832e47cdf4
Marina Taques <marinataques27@gmail.com>
2022-01-06 19:54:21 -0300

    Add files via upload
    aprendendo a validar

--- Titanic-1st_project.py
+++ Titanic-1st_project.py
@@ -1,53 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[106]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[3]:
+# In[107]:
 
 
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
 
-# In[5]:
+# In[108]:
 
 
 train.head()
 
 
-# In[62]:
+# In[109]:
 
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
-# In[9]:
-
-
-modelo = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
-
-
-# In[13]:
-
-
-train['Sex'].value_counts()
-
-
-# In[ ]:
-
-
-TREINO
-VALIDACAO
-TESTE
-
-
-# In[25]:
+# In[110]:
 
 
 def transformar_sexo(valor):
@@ -59,50 +40,72 @@ train ['Sex_binario'] = train ['Sex'].map(transformar_sexo)
 test ['Sex_binario'] = test['Sex'].map(transformar_sexo)
 
 
-# In[16]:
+# In[111]:
 
 
 train.head()
 
 
-# In[11]:
+# In[112]:
 
 
 variaveis = ['Sex_binario', 'Age']
 
 
-# In[26]:
+# In[113]:
 
 
 x = train[variaveis]
 y = train['Survived']
 
 
-# In[19]:
+# In[114]:
 
 
-x.head()
+np.random.seed(0)
+x_treino, x_valid, y_treino, y_valid = train_test_split(x, y, test_size = 0.5)
 
 
-# In[20]:
+# In[115]:
 
 
-y.head()
+x_treino.shape, x_valid.shape, y_treino.shape, y_valid.shape
 
 
-# In[29]:
+# In[116]:
 
 
-x = x.fillna(-1)
+x_treino = x_treino.fillna(-1)
+x_valid = x_valid.fillna(-1)
 
 
-# In[30]:
+# In[117]:
 
 
-modelo.fit(x,y)
+modelo = RandomForestClassifier(n_estimators=100, n_jobs=-1, random_state=0)
+modelo.fit(x_treino, y_treino)
 
 
-# In[40]:
+# In[118]:
+
+
+p = modelo.predict(x_valid)
+
+
+# In[119]:
+
+
+np.mean(y_valid == p)
+
+
+# In[120]:
+
+
+p = (x_valid['Sex_binario']==1).astype(np.int64)
+np.mean(y_valid == p)
+
+
+# In[121]:
 
 
 x_prev = test[variaveis]
@@ -110,33 +113,33 @@ x_prev = x_prev.fillna(-1)
 x_prev.head()
 
 
-# In[42]:
+# In[122]:
 
 
 p = modelo.predict(x_prev)
 p
 
 
-# In[43]:
+# In[123]:
 
 
 test.head()
 
 
-# In[56]:
+# In[124]:
 
 
 sub = pd.Series(p, index=test['PassengerId'], name='Survived')
 sub.shape
 
 
-# In[53]:
+# In[125]:
 
 
 sub.to_csv("primeiro_modelo.csv", header=True)
 
 
-# In[54]:
+# In[126]:
 
 
 get_ipython().system('head -n10 primeiro_modelo.csv')
